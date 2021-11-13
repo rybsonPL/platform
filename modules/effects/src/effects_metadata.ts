df8e67fefa6dc76452d1bad890921c74cf8f269c
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-11-13 22:21:14 +0100

    feat(effects): remove @Effect decorator
    BEFORE:
    
    @Effect()
    fetch$ = this.actions$.pipe(...)
    
    AFTER:
    
    The createEffect method replaces the @Effect decorator.
    
    fetch$ = createEffect(() => this.actions$.pipe(...))

--- modules/effects/src/effects_metadata.ts
+++ modules/effects/src/effects_metadata.ts
@@ -1,6 +1,5 @@
 import { EffectMetadata, EffectsMetadata } from './models';
 import { getCreateEffectMetadata } from './effect_creator';
-import { getEffectDecoratorMetadata } from './effect_decorator';
 
 export function getEffectsMetadata<T>(instance: T): EffectsMetadata<T> {
   return getSourceMetadata(instance).reduce(
@@ -16,13 +15,5 @@ export function getEffectsMetadata<T>(instance: T): EffectsMetadata<T> {
 }
 
 export function getSourceMetadata<T>(instance: T): EffectMetadata<T>[] {
-  const effects: Array<(instance: T) => EffectMetadata<T>[]> = [
-    getEffectDecoratorMetadata,
-    getCreateEffectMetadata,
-  ];
-
-  return effects.reduce<EffectMetadata<T>[]>(
-    (sources, source) => sources.concat(source(instance)),
-    []
-  );
+  return getCreateEffectMetadata(instance);
 }
